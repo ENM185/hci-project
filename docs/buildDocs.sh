e995d060c68147eda6cf3c7dde51411200c44624
ENM185 <ericnathanmiller@icloud.com>
2021-03-06 21:35:19 -0500

    Fix custom domain name issue

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -69,6 +69,9 @@ For more information on how this documentation is built using Sphinx, Read the D
  
  * https://tech.michaelaltfield.net/2020/07/18/sphinx-rtd-github-pages-1
 EOF
+
+# Keeps custom url
+echo "lightningsharks.ga" >> CNAME
  
 # copy the resulting html pages built from sphinx above to our new git repo
 git add .
